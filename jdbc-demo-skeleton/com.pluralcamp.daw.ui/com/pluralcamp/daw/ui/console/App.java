17246a725d11b0e9b2140aff1bd9332915ffd477
cgonzalez21ilg <cgonzalez21@ilg.cat>
2023-05-05 18:38:12 +0200

    commit_05/05/2023

--- jdbc-demo-skeleton/com.pluralcamp.daw.ui/com/pluralcamp/daw/ui/console/App.java
+++ jdbc-demo-skeleton/com.pluralcamp.daw.ui/com/pluralcamp/daw/ui/console/App.java
@@ -2,16 +2,18 @@ package com.pluralcamp.daw.ui.console;
 
 import java.util.Scanner;
 
+import org.w3c.dom.events.Event;
+
 public class App {
     public static void main(String[] args) {
        
         Scanner stdin = new Scanner (System.in);
 
-        ColorJDBCDAO colorDAO = new ColorJDBCDAO();
-        EventJDBCDAO eventDAO = new EventJDBCDAO();
+        ColorJDBCDAOImpl colorDAO = new ColorJDBCDAOImpl();
+        EventJDBCDAOImpl eventDAO = new EventJDBCDAOImpl();
 
         try{
-            Color c =colorDAO.getColorById(5);
+            var c = Color c =colorDAO.getColorById(5);
             if(c != null) {
                 System.out.println(c.toString());
             }
@@ -27,6 +29,36 @@ public class App {
             for (Color c : colors) {
                 System.out.println(c.toString());
             }
+        } catch (DAOException ex) {
+            System.out.printf("Error:: %s %n", ex.getMessage());
         }
+
+        System.out.println("Pulsa una tecla para continuar");
+        stdin.nextLine();
+
+        try {
+            Event e = eventDAO.getEventById(5);
+            if (e != null) {
+                System.out.println(e.toString());
+            }
+        } catch (DAOException ex) {
+            System.out.printf("Error:: %s %n", ex.getMessage());
+        }
+
+        System.out.println("Pulsa una tecla para continuar");
+        stdin.nextLine();
+
+        try {
+            List<Event> events = eventDAO.getEvents();
+            for (Event e : events){
+                System.out.println(e.toString());
+            }
+        } catch (DAOException ex) {
+            System.out.printf("Error:: %s %n", ex.getMessage());
+        }
+
+        System.out.println("Pulsa una tecla para continuar");
+        stdin.nextLine();
+
     }
 }
