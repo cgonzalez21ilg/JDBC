518e2e8b8154f004c3cececc00f26e288787fa70
cgonzalez21ilg <cgonzalez21@ilg.cat>
2023-05-19 18:09:19 +0200

    Commit 19/05/2023

--- jdbc-demo-skeleton/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/daos/contracts/EventDAO.java
+++ jdbc-demo-skeleton/com.pluralcamp.daw.persistence/com/pluralcamp/daw/persistence/daos/contracts/EventDAO.java
@@ -1,16 +1,14 @@
 package com.pluralcamp.daw.persistence.daos.contracts;
-
 import com.pluralcamp.daw.entities.core.Event;
 import com.pluralcamp.daw.persistence.exceptions.DAOException;
-
 import java.util.List;
 
 public interface EventDAO {
     Event getEventById(long id) throws DAOException;
     List<Event> getEvents() throws DAOException;
     List<Event> getEvents(int offset, int count) throws DAOException;
-    List<Event> getEvents(boolean visible) throws DAOException;
-    List<Event> getEvents(boolean visible, int offset, int count) throws DAOException;
+    //List<Event> getEvents(boolean visible) throws DAOException;
+    //List<Event> getEvents(boolean visible, int offset, int count) throws DAOException;
     List<Event> getEvents(String searchTerm) throws DAOException;
     List<Event> getEvents(String searchTerm, int offset, int count) throws DAOException;
 }
